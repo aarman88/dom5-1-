9507ea2e9092aa0f3bd8605fc15ab3c22b6835db
Арман <@LAPTOP-8BLD2IN5>
2023-10-15 22:50:12 +0600

    Добавьте файлы проекта.

---
+++ dom5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+
+class Program
+{
+    static void Main()
+    {
+        string url = "https://example.com/nonexistentpage"; // Замените на адрес ресурса, который не существует
+
+        try
+        {
+            WebClient client = new WebClient();
+            string result = client.DownloadString(url);
+
+            // Если запрос успешен, вы можете здесь обрабатывать полученные данные
+            Console.WriteLine(result);
+        }
+        catch (WebException ex)
+        {
+            Console.WriteLine("Произошла ошибка при запросе к веб-ресурсу:");
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Произошла неизвестная ошибка:");
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
